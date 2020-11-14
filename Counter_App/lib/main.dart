70193532e3ba53d0e753d957928bf0827dbd6074
Haroon khan <haroonkhan9426@gmail.com>
2020-11-14 15:18:02 +0500

    Counter App completed

--- Counter_App/lib/main.dart
+++ Counter_App/lib/main.dart
@@ -7,30 +7,49 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: HomeScreen());
+    return MaterialApp(
+      home: HomeScreen(),
+    );
   }
 }
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int count = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.white,
-        appBar: AppBar(title: Text('Home Screen')),
-        floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.add),
-          onPressed: () {
-            print('Add button pressed');
-          },
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Text('Counter App'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          setState(() {
+            count++;
+          });
+          print('Count is: $count');
+        },
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text('You have pushed the button this many times'),
+            Text(
+              count.toString(),
+              style: TextStyle(fontSize: 25, color: Colors.black),
+            ),
+          ],
         ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text('Number of counts'),
-              Text('0'),
-            ],
-          ),
-        ));
+      ),
+    );
   }
 }
